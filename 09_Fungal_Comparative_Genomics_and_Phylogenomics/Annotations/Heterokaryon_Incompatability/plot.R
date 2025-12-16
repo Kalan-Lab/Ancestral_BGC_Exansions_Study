c1d13429d220a0b5ef89ec7b124d88eaedc460b1
Rauf Salamzade <salamzader@gmail.com>
2025-12-16 17:15:42 +0000

    update for revisions

--- 09_Fungal_Comparative_Genomics_and_Phylogenomics/Annotations/Heterokaryon_Incompatability/plot.R
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-
-dat <- read.table("Plotting_Input.txt", header=T, sep='\t')
-
-pdf("HetProp_SupFigure.pdf", height=4, width=2)
-ggplot(dat, aes(x=group, y=(hi_prop*100.0))) + geom_boxplot() + theme_bw()  + theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust=1)) + ylab("Percentage of total proteins\nwith HI domain") + xlab("") 
-dev.off()
-
-
