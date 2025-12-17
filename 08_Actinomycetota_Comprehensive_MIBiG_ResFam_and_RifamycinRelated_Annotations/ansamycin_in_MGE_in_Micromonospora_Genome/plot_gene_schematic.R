c1448655176701d4061efa6ef21f72646fc7b1c8
Rauf Salamzade <salamzader@gmail.com>
2025-12-16 22:01:38 -0800

    Remove unused color categories from gene schematic

--- 08_Actinomycetota_Comprehensive_MIBiG_ResFam_and_RifamycinRelated_Annotations/ansamycin_in_MGE_in_Micromonospora_Genome/plot_gene_schematic.R
+++ 08_Actinomycetota_Comprehensive_MIBiG_ResFam_and_RifamycinRelated_Annotations/ansamycin_in_MGE_in_Micromonospora_Genome/plot_gene_schematic.R
@@ -15,12 +15,9 @@ annotation_colors <- c(
   "PKS" = "#C00000",                   # Red
   "NRPS(-like)" = "#BDC3C7",           # Gray (same as Other)
   "Cytochrome P450" = "#BDC3C7",       # Gray (same as Other)
-  "Transporter" = "#3498DB",           # Blue
   "Transposase / Putative Transposase" = "#5DADE2", # Light blue
   "Reverse transcriptase" = "#2E86C1", # Medium blue
   "Putative phage integrase" = "#1B4F72", # Dark blue
-  "Tailoring enzyme" = "#F39C12",      # Orange/Yellow
-  "Regulator" = "#2ECC71",             # Green
   "Other" = "#BDC3C7"                  # Gray
 )
 
@@ -32,7 +29,7 @@ genes <- read_tsv(INPUT_FILE, show_col_types = FALSE)
 genes <- genes %>%
   mutate(
     display_category = case_when(
-      manual_annotation %in% c("Cytochrome P450", "Cytochrome 450", "NRPS(-like)") ~ "Other",
+      manual_annotation %in% c("Cytochrome P450", "NRPS(-like)") ~ "Other",
       TRUE ~ manual_annotation
     ),
     direction = ifelse(strand == "+", 1, -1)
